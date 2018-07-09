caad10fa92d0b56605c264d60b6e635a1c102320
Taylor Dawson <taylorjdawson@gmail.com>
2018-07-09 13:33:59 -0700

    [W.I.P.] Modified util.js for new data schema:   - getReg & writeReg now work with .yaml file   - Removing ignored files

--- node/register.js
+++ node/register.js
@@ -5,5 +5,5 @@ var Utils = require('./utils');
 module.exports = function(key) {
   var existingFile = Utils.getRegFile();
   Utils.writeToRegFile(existingFile + key + '\n');
-  console.log('[notify] Your registration code has been saved to ~/.notifyreg');
+  console.log('[notify] Your registration code has been saved to ~/.notifyreg.yml');
 };
--- node/utils.js
+++ node/utils.js
@@ -1,23 +1,28 @@
 var fs = require('fs');
+const yaml = require('js-yaml');
 
 var getUserHome = function() {
   return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 };
 
 var getRegFilename = function() {
-  return getUserHome() + '/.notifyreg';
+  return getUserHome() + '/.notifyreg.yml';
 };
 
 module.exports = {
+
+  // Returns JSON object
   getRegFile: function() {
     if (fs.existsSync(getRegFilename())) {
-      return fs.readFileSync(getRegFilename(), {encoding: 'utf8'});
+      return yaml.safeLoad(fs.readFileSync(getRegFilename(), {encoding: 'utf8'}));
     }
-
-    return '';
+	
+	// Create the regFile schema since it doesn't exist
+    return {'keys': {}, 'aliases': {}};
   },
 
-  writeToRegFile: function(text) {
-    fs.writeFileSync(getRegFilename(), text);
+  writeToRegFile: function(data) {
+  	fs.writeFileSync(getRegFilename(), yaml.safeDump(data), 'utf-8');
   },
 };
+
